6dae2240df77ad5eb5db1e2f842716b0c9df700a
Satoru Kameyama <kame3niku9@gmail.com>
2020-02-22 18:20:43 +0900

    update: implement like anser video, but TLE

--- ABC155/D/main.py
+++ ABC155/D/main.py
@@ -1,52 +1,54 @@
-import math
-
 def resolve():
     N, K = map(int, input().split())
     A = list(map(int, input().split()))
-    
+
     A.sort()
+    INF = 10**18 + 1
 
-    plus = []
-    minus = []
-    zeros = []
-    for a in A:
-        if a < 0:
-            minus.append(a)
-        elif a == 0:
-            zeros.append(a)
+    # 「積がx未満となるペアの個数がK個未満」となる最大のxを求める
+    l =  -INF  # OK
+    r = INF  # NG
+
+    # 二分探索 O(logN)
+    def loop():
+        total = 0
+        for i in range(N):
+            if A[i] < 0:
+                l = -1
+                r = N
+                while l < r - 1:
+                    center = (l + r) // 2
+                    if A[center] * A[i] < x:
+                        r = center
+                    else:
+                        l = center
+                total += N - r
+            else:
+                l = -1
+                r = N
+                while l < r - 1:
+                    center = (l + r) // 2
+                    if A[center] * A[i] < x:
+                        l = center
+                    else:
+                        r = center
+                total += r
+            if A[i] * A[i] < x:
+                total -= 1
+        total /= 2
+        return total < K
+        
+    # 尺取り法 O(n)
+    def loop2():
+
+
+    while l < r - 1:
+        x = (l + r) // 2
+        if loop():
+            l = x
         else:
-            plus.append(a)
-
-    def combinations_count(n, r):
-        if n < r:
-            return 0
-        return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
-
-    minus_num = len(plus) * len(minus)
-    zeros_num = sum([len(A) - i - 1 for i in range(len(zeros))])
-    plus_num = combinations_count(len(plus), 2) + combinations_count(len(minus), 2)
-
-    if K <= minus_num:
-        m_idx = K // len(minus)
-        p_idx = K % len(minus)
-        minus.sort(reverse=True)
-        plus.sort(reverse=True)
-        res = minus[m_idx - 1] * plus[p_idx - 1]
-    elif K <= zeros_num:
-        res = 0
-    else:
-        store = []
-        for i in range(len(plus)):
-            for j in range(i+1, len(plus)):
-                store.append(plus[i] * plus[j])
-        for i in range(len(minus)):
-            for j in range(i+1, len(minus)):
-                store.append(minus[i] * minus[j])
-        store.sort()
-        idx = K - (minus_num + zeros_num)
-        res = store[idx - 1]        
-
-    print(res)
+            r = x
+    print(l)
 
 
 if __name__ == "__main__":
