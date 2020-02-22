1cb0f0d3e34e627557563839cc108d76391c77bc
Satoru Kameyama <kame3niku9@gmail.com>
2020-02-22 23:33:12 +0900

    update: ABC156 D passed

--- ABC156/D/main.py
+++ ABC156/D/main.py
@@ -1,74 +1,25 @@
-"""
-import math
 
-def combinations_count(n, r):
-    return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
+def mod_inverse(a, p):
+    return pow(a, p-2, p)  # フェルマーの小定理より
 
-from operator import mul
-from functools import reduce
+def mod_binominal(n, r, p):
+    numerator= 1
+    for i in range(r):
+        numerator = (numerator * (n - i)) % p
 
+    denominator = 1
+    for i in range(1, r+1):
+        denominator = (denominator * i) % p
 
-def combinations_count(n, r):
-    r = min(r, n - r)
-    numer = reduce(mul, range(n, n - r, -1), 1)
-    denom = reduce(mul, range(1, r + 1), 1)
-    return numer // denom
-
-
-def combinations_count(n, r):
-    if n - r < r: r = n - r
-    if r == 0: return 1
-    if r == 1: return n
-
-    numerator = [n - r + k + 1 for k in range(r)]
-    denominator = [k + 1 for k in range(r)]
-
-    for p in range(2,r+1):
-        pivot = denominator[p - 1]
-        if pivot > 1:
-            offset = (n - r) % p
-            for k in range(p-1,r,p):
-                numerator[k - offset] /= pivot
-                denominator[k] /= pivot
-
-    result = 1
-    for k in range(r):
-        if numerator[k] > 1:
-            result *= int(numerator[k])
-
-    return result
-"""
+    return (numerator * mod_inverse(denominator, p)) % p
 
 def resolve():
     n, a, b = map(int, input().split())
+    p = 10**9+7
 
-    all = 2 ** n
+    ans = pow(2, n, p) - mod_binominal(n, a, p) - mod_binominal(n, b, p) - 1
+    print((ans + p) % p)
 
-    mod = 10**9+7 #出力の制限
-    N = n
-    g1 = [1, 1] # 元テーブル
-    g2 = [1, 1] #逆元テーブル
-    inverse = [0, 1] #逆元テーブル計算用テーブル
-
-    for i in range( 2, N + 1 ):
-        g1.append( ( g1[-1] * i ) % mod )
-        inverse.append( ( -inverse[mod % i] * (mod//i) ) % mod )
-        g2.append( (g2[-1] * inverse[-1]) % mod )
-
-    def combinations_count(n, r, mod):
-        if ( r<0 or r>n ):
-            return 0
-        r = min(r, n-r)
-        return g1[n] * g2[r] * g2[n-r] % mod
-
-    all -= combinations_count(n, a, mod)
-    all -= combinations_count(n, b, mod)
-
-    if n==2 and a != b:
-        print(0)
-        return 
-
-    print(all - 1)
 
 if __name__ == "__main__":
     resolve()
