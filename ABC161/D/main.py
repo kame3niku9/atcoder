8d9de2a90d384be1cc03d303f914b4006230cb1e
Satoru Kameyama <kame3niku9@gmail.com>
2020-04-05 00:46:04 +0900

    add: ABC161/D

--- ABC161/D/main.py
+++ ABC161/D/main.py
@@ -1,27 +1,28 @@
 import numpy as np
 def resolve():
     k = int(input())
-    n = 3234566667
-    l = []
-    i = 1
-    while i < 13: 
-        l.append(i)
-        i += 1
-        
-    i = 21
-    while i < n:
-        a = list(map(int, str(i)))
-        m = max(abs(np.diff(a)))
-        if m <= 1:
-            l.append(i)
-            i += 1
-        else:
-            i += 8
-    print(l)
-    print(l[k-1])
+    n = 20
+    l = [[] for _ in range(n)]
+    size = 0
+    pre_size = 0
 
+    for i in range(1, 10, 1):
+        l[0].append(i)
+    
+    size += len(l[0])
+    for j in range(n-1):
+        if size >= k:
+            break
+        for var in l[j]:
+            v = var % 10
+            for a in range(v-1, v+2, 1):
+                #print(a)
+                if a >= 0 and a <= 9:
+                    l[j+1].append(var*10 + a)
+        size += len(l[j+1])
+        pre_size += len(l[j])
 
-
+    print(l[j][k - pre_size - 1])
 
 if __name__ == "__main__":
     resolve()
